86aa1b2530e096f68b47916b4a1872866c49e4f6
magonn <gotman.maria@gmail.com>
2015-11-06 16:02:09 +0300

    Update examples

--- demo/denoising.lua
+++ demo/denoising.lua
@@ -2,20 +2,12 @@ require 'cv.photo'
 require 'cv.imgcodecs'
 require 'cv.highgui'
 
-local image1 = cv.imread(arg[1], cv.CV_8UC1)
-local image2 = cv.imread(arg[2], cv.CV_8UC1)
-
-local dst_image1 = image1 * 0
+local image1 = cv.imread{arg[1]}
+local image2 = cv.imread{arg[2]}
 
 image = {image1, image2}
 
---cv.fastNlMeansDenoising{src = image1, dst = dst_image1, h = 5, templateWindowSize = 7, searchWindowSize = 21}
---cv.fastNlMeansDenoisingColored{src = image1, dst = dst_image1, h = 5, hColor = 5, templateWindowSize = 7, searchWindowSize = 21}
+dst = cv.fastNlMeansDenoisingColoredMulti{image, nil, 1, 1};
 
---cv.fastNlMeansDenoisingMulti{srcImgs = image, dst = dst_image1, imgToDenoiseIndex = 1, temporalWindowSize = 1};
-cv.fastNlMeansDenoisingColoredMulti{srcImgs = image, dst = dst_image1, imgToDenoiseIndex = 1, temporalWindowSize = 1};
-
-cv.imshow{winname="Inpaint image", image=dst_image1}
+cv.imshow{winname = "Denoising image", image = dst}
 cv.waitKey{0}
---cv.imshow("Inpaint image", dst_image2)
---cv.waitKey(0)
--- demo/inpainting.lua
+++ demo/inpainting.lua
@@ -7,8 +7,8 @@ if not arg[1] or not arg[2] then
     os.exit(0)
 end
 
-local image = cv.imread(arg[1])
-local mask = cv.imread(arg[2], cv.CV_8UC1)
+local image = cv.imread{arg[1]}
+local mask = cv.imread{arg[2], cv.CV_8UC1}
 
 local dst_image = image * 0
 cv.inpaint{src = image, inpaintMask = mask, dst = dst_image, inpaintRadius = 1, flags = cv.INPAINT_TELEA}
