f5424763296b9ad7061d8d7f0f838db0a5bdb2a3
Egor Burkov <shrubb@ya.ru>
2015-11-25 13:08:12 +0300

    Lena-related bugfix in demo/LineSegmentDetector.lua

--- demo/LineSegmentDetector.lua
+++ demo/LineSegmentDetector.lua
@@ -4,11 +4,11 @@ require 'cv.imgproc'
 require 'cv.imgcodecs'
 
 if not arg[1] then
-	print('Usage: `th demo/LineSegmentDetector.lua path-to-image`\n')
+	print('Usage: `th demo/LineSegmentDetector.lua path-to-image`')
 	print('Now using demo/lena.jpg')
 end
 
-local image = cv.imread{arg[1], cv.IMREAD_GRAYSCALE}
+local image = cv.imread{arg[1] or 'demo/lena.jpg', cv.IMREAD_GRAYSCALE}
 
 local detector = cv.LineSegmentDetector{}
 local lines = detector:detect{image}
