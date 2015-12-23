f2444422eaa9e9c89de4574a36fc99d6396b2869
Egor Burkov <shrubb@ya.ru>
2015-12-23 17:43:52 +0300

    cudaarithm headers

--- cv/cudaarithm/init.lua
+++ cv/cudaarithm/init.lua
@@ -8,6 +8,76 @@ local ffi = require 'ffi'
 ffi.cdef[[
 struct TensorWrapper min(
         void *state, struct TensorWrapper src1, struct TensorWrapper src2, struct TensorWrapper dst);
+
+struct TensorWrapper max(
+        void *state, struct TensorWrapper src1, struct TensorWrapper src2, struct TensorWrapper dst);
+
+struct TensorWrapper threshold(
+        void *state, struct TensorWrapper src, 
+        struct TensorWrapper dst, double thresh, double maxval, int type);
+
+struct TensorWrapper magnitude(
+        void *state, struct TensorWrapper xy, struct TensorWrapper magnitude);
+
+struct TensorWrapper magnitudeSqr(
+        void *state, struct TensorWrapper xy, struct TensorWrapper magnitude);
+
+struct TensorWrapper magnitude2(
+        void *state, struct TensorWrapper x, struct TensorWrapper y, struct TensorWrapper magnitude);
+
+struct TensorWrapper magnitudeSqr2(
+        void *state, struct TensorWrapper x, struct TensorWrapper y, struct TensorWrapper magnitudeSqr);
+
+struct TensorWrapper phase(
+        void *state, struct TensorWrapper x, struct TensorWrapper y,
+        struct TensorWrapper angle, bool angleInDegrees);
+
+struct TensorArray cartToPolar(
+        void *state, struct TensorWrapper x, struct TensorWrapper y,
+        struct TensorWrapper magnitude, struct TensorWrapper angle, bool angleInDegrees);
+
+struct TensorArray polarToCart(
+        void *state, struct TensorWrapper magnitude, struct TensorWrapper angle,
+        struct TensorWrapper x, struct TensorWrapper y, bool angleInDegrees);
+
+struct PtrWrapper LookUpTable_ctor(
+        void *state, struct TensorWrapper lut);
+
+struct TensorWrapper LookUpTable_transform(
+        void *state, struct PtrWrapper ptr,
+        struct TensorWrapper src, struct TensorWrapper dst);
+
+struct TensorWrapper rectStdDev(
+        void *state, struct TensorWrapper src, struct TensorWrapper sqr,
+        struct TensorWrapper dst, struct RectWrapper rect);
+
+struct TensorWrapper normalize(
+        void *state, struct TensorWrapper src, struct TensorWrapper dst,
+        double alpha, double beta, int norm_type, int dtype);
+
+struct TensorWrapper integral(
+        void *state, struct TensorWrapper src, struct TensorWrapper sum);
+
+struct TensorWrapper sqrIntegral(
+        void *state, struct TensorWrapper src, struct TensorWrapper sum);
+
+struct TensorWrapper mulSpectrums(
+        void *state, struct TensorWrapper src1, struct TensorWrapper src2, 
+        struct TensorWrapper dst, int flags, bool conjB);
+
+struct TensorWrapper mulAndScaleSpectrums(
+        void *state, struct TensorWrapper src1, struct TensorWrapper src2,
+        struct TensorWrapper dst, int flags, float scale, bool conjB);
+
+struct TensorWrapper dft(
+        void *state, struct TensorWrapper src, 
+        struct TensorWrapper dst, struct SizeWrapper dft_size, int flags);
+
+struct PtrWrapper Convolution_ctor(struct SizeWrapper user_block_size);
+
+struct TensorWrapper Convolution_convolve(
+        void *state, struct PtrWrapper ptr, struct TensorWrapper image,
+        struct TensorWrapper templ, struct TensorWrapper result, bool ccor);
 ]]
 
 local C = ffi.load(cv.libPath('cudaarithm'))
--- include/cudaarithm.hpp
+++ include/cudaarithm.hpp
@@ -17,3 +17,97 @@ struct ConvolutionPtr {
     inline ConvolutionPtr(cuda::Convolution *ptr) { this->ptr = ptr; }
     inline operator const cuda::Convolution &() { return *static_cast<cuda::Convolution *>(ptr); }
 };
+
+extern "C"
+struct TensorWrapper min(
+        struct THCState *state, struct TensorWrapper src1, struct TensorWrapper src2, struct TensorWrapper dst);
+
+extern "C"
+struct TensorWrapper max(
+        struct THCState *state, struct TensorWrapper src1, struct TensorWrapper src2, struct TensorWrapper dst);
+
+extern "C"
+struct TensorWrapper threshold(
+        struct THCState *state, struct TensorWrapper src,
+        struct TensorWrapper dst, double thresh, double maxval, int type);
+
+extern "C"
+struct TensorWrapper magnitude(
+        struct THCState *state, struct TensorWrapper xy, struct TensorWrapper magnitude);
+
+extern "C"
+struct TensorWrapper magnitudeSqr(
+        struct THCState *state, struct TensorWrapper xy, struct TensorWrapper magnitude);
+
+extern "C"
+struct TensorWrapper magnitude2(
+        struct THCState *state, struct TensorWrapper x, struct TensorWrapper y, struct TensorWrapper magnitude);
+
+extern "C"
+struct TensorWrapper magnitudeSqr2(
+        struct THCState *state, struct TensorWrapper x, struct TensorWrapper y, struct TensorWrapper magnitudeSqr);
+
+extern "C"
+struct TensorWrapper phase(
+        struct THCState *state, struct TensorWrapper x, struct TensorWrapper y,
+        struct TensorWrapper angle, bool angleInDegrees);
+
+extern "C"
+struct TensorArray cartToPolar(
+        struct THCState *state, struct TensorWrapper x, struct TensorWrapper y,
+        struct TensorWrapper magnitude, struct TensorWrapper angle, bool angleInDegrees);
+
+extern "C"
+struct TensorArray polarToCart(
+        struct THCState *state, struct TensorWrapper magnitude, struct TensorWrapper angle,
+        struct TensorWrapper x, struct TensorWrapper y, bool angleInDegrees);
+
+extern "C"
+struct LookUpTablePtr LookUpTable_ctor(
+        struct THCState *state, struct TensorWrapper lut);
+
+extern "C"
+struct TensorWrapper LookUpTable_transform(
+        struct THCState *state, struct LookUpTablePtr ptr,
+        struct TensorWrapper src, struct TensorWrapper dst);
+
+extern "C"
+struct TensorWrapper rectStdDev(
+        struct THCState *state, struct TensorWrapper src, struct TensorWrapper sqr,
+        struct TensorWrapper dst, struct RectWrapper rect);
+
+extern "C"
+struct TensorWrapper normalize(
+        struct THCState *state, struct TensorWrapper src, struct TensorWrapper dst,
+        double alpha, double beta, int norm_type, int dtype);
+
+extern "C"
+struct TensorWrapper integral(
+        struct THCState *state, struct TensorWrapper src, struct TensorWrapper sum);
+
+extern "C"
+struct TensorWrapper sqrIntegral(
+        struct THCState *state, struct TensorWrapper src, struct TensorWrapper sum);
+
+extern "C"
+struct TensorWrapper mulSpectrums(
+        struct THCState *state, struct TensorWrapper src1, struct TensorWrapper src2,
+        struct TensorWrapper dst, int flags, bool conjB);
+
+extern "C"
+struct TensorWrapper mulAndScaleSpectrums(
+        struct THCState *state, struct TensorWrapper src1, struct TensorWrapper src2,
+        struct TensorWrapper dst, int flags, float scale, bool conjB);
+
+extern "C"
+struct TensorWrapper dft(
+        struct THCState *state, struct TensorWrapper src,
+        struct TensorWrapper dst, struct SizeWrapper dft_size, int flags);
+
+extern "C"
+struct ConvolutionPtr Convolution_ctor(struct SizeWrapper user_block_size);
+
+extern "C"
+struct TensorWrapper Convolution_convolve(
+        struct THCState *state, struct ConvolutionPtr ptr, struct TensorWrapper image,
+        struct TensorWrapper templ, struct TensorWrapper result, bool ccor);
