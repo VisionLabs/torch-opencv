47f2ca0060433bc2c58e8e35d99bfe2f9717ac29
Egor Burkov <shrubb@ya.ru>
2016-02-20 22:11:05 +0300

    Polish demo a bit

--- demo/calibrateCamera.lua
+++ demo/calibrateCamera.lua
@@ -11,24 +11,38 @@ local imageSize = cv.Size(640, 360)
 local img = {}
 
 for i = 1, numImages do
-    img[i] = cv.imread{'demo/templates/template' .. i .. '.jpg'}
+    img[i] = cv.imread{'demo/templates/template'..i..'.jpg'}
 end
 
 local corners = {}
 local isFound = false
 
-for i = 1,numImages do
-    isFound, corners[i] = cv.findChessboardCorners{image = img[i], patternSize = patternSize }
-    print(isFound)
-    local img_gray = cv.cvtColor{src = img[i], code = cv.COLOR_BGR2GRAY }
-    cv.cornerSubPix{image = img_gray, corners = corners[i], winSize = cv.Size(11, 11), zeroZone = cv.Size(-1, -1),
-                    criteria = cv.TermCriteria(cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.1)}
-    cv.drawChessboardCorners{image = img[i], patternSize = patternSize, corners = corners[i], patternWasFound = isFound}
-    cv.imshow{winname="template", image = img[i]}
-    cv.waitKey{}
+for i = 1, numImages do
+    isFound, corners[i] = cv.findChessboardCorners{image = img[i], patternSize = patternSize}
+    if not isFound then
+        print('Chessboard at image #'..i..' was NOT found!')
+    end
+
+    local img_gray = cv.cvtColor{img[i], code = cv.COLOR_BGR2GRAY}
+    cv.cornerSubPix{
+        image    = img_gray,
+        corners  = corners[i],
+        winSize  = cv.Size(11, 11), 
+        zeroZone = cv.Size(-1, -1),
+        criteria = {cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.1}
+    }
+    cv.drawChessboardCorners{
+        image           = img[i],
+        patternSize     = patternSize,
+        corners         = corners[i],
+        patternWasFound = isFound
+    }
+    cv.imshow{"template", img[i]}
+    cv.waitKey{0}
 end
 
 local objectPoints = {}
+
 for k = 1, numImages do
     objectPoints[k] = torch.FloatTensor(patternSize.height * patternSize.width, 1, 3)
     for i = 1, patternSize.height do
@@ -40,14 +54,19 @@ for k = 1, numImages do
     end
 end
 
-local retval, cameraMatrix, distCoeffs, rvecs, tvecs =
-                cv.calibrateCamera{objectPoints = objectPoints, imagePoints = corners,
-                                imageSize = imageSize }
+local error, cameraMatrix, distCoeffs, rvecs, tvecs =
+    cv.calibrateCamera{
+        objectPoints = objectPoints, 
+        imagePoints  = corners,
+        imageSize    = imageSize
+    }
 
+print('Final re-projection error: '..error)
 
 local src = cv.imread{'demo/templates/image.jpg'}
+cv.imshow{"Photo to undistort", src}
+cv.waitKey{0}
 
-local dst = cv.undistort{src = src, cameraMatrix = cameraMatrix, distCoeffs = distCoeffs}
-
-cv.imshow{winname = "undistorted image", image = dst}
-cv.waitKey{}
+local dst = cv.undistort{src, cameraMatrix, distCoeffs}
+cv.imshow{"Photo to undistort", dst}
+cv.waitKey{0}
