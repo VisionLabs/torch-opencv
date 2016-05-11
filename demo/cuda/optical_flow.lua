f9f746925443d3c7ada158334a3cc14f26a3995e
Egor Burkov <shrubb@ya.ru>
2016-05-11 19:07:34 +0300

    Update optical flow demo & missing pictures

--- demo/cuda/optical_flow.lua
+++ demo/cuda/optical_flow.lua
@@ -2,8 +2,11 @@
 if not arg[1] or not arg[2] then
     print ('Usage: '..arg[0]..' image1.png image2.png')
     print ('ex: '..arg[0]..' ../data/basketball1.png ../data/basketball2.png')
-    os.exit(0)
+    print ('Now using demo/data/basketball*.png')
+    arg[1] = 'demo/data/basketball1.png'
+    arg[2] = 'demo/data/basketball2.png'
 end
+
 local cv = require 'cv'
 require 'cv.cudaoptflow'
 require 'cv.imgcodecs'
@@ -11,7 +14,7 @@ require 'cv.imgproc'
 require 'cv.highgui'
 require 'image'
 
---different method expect different range of values
+-- different method expect different range of values
 local flowMethods={
     Brox     ={computer=cv.cuda.BroxOpticalFlow{},      multiplier=1},
     TVL1     ={computer=cv.cuda.OpticalFlowDual_TVL1{}, multiplier=1},
@@ -27,12 +30,15 @@ function alignUp(x, alignBoundary)
     return strideBytes/x:elementSize()
 end
 
---load images and copy into memory-aligned CudaTensor
+-- load images and copy into memory-aligned CudaTensor
 cpuImages={};
 gpuImages={}
+
 for i=1, 2 do
-    cpuImages[i]=cv.imread{arg[i], cv.IMREAD_GRAYSCALE}:float();
-    cpuImages[i]=cpuImages[i]/cpuImages[i]:max()
+    cpuImages[i]=cv.imread{arg[i], cv.IMREAD_GRAYSCALE}
+    assert(cpuImages[i]:nDimension() > 0, 'Could not load '..arg[i])
+
+    cpuImages[i]=cpuImages[i]:float():div(255)
     local sizes=cpuImages[i]:size()
     local strides=torch.LongStorage{alignUp(cpuImages[i]), -1}
     gpuImages[i]=torch.CudaTensor(sizes, strides)
@@ -48,7 +54,6 @@ for key, flowMethod in pairs(flowMethods) do
     -- color model ranges H:0-360, S:0-1, V:0-1 --> R:0-1, G:0-1, V:0-1
     local flowHSV=torch.cat({flowAng*180/math.pi+180, flowMag/flowMag:max(), torch.FloatTensor():ones(flowMag:size())}, 3)
     local flowRGB=(cv.cvtColor{src=flowHSV, code=cv.COLOR_HSV2RGB}*255):byte()
-    cv.imwrite{filename=key..'.png', img=flowRGB}
     cv.imshow{key, flowRGB}
 end
 cv.waitKey{0}
